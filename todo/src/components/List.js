4cdf118fa8f17511378e89b8d768af3bd482be81
Tusshar <tpaul.be21@gmail.com>
2022-10-25 10:54:41 +0530

    todo project

--- todo/src/components/List.js
+++ todo/src/components/List.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import DeleteIcon from '@mui/icons-material/Delete';
 
-const List = () => {
+const List = ({id,text,dltitems}) => {
   return (
-    <div>List</div>
+    <>
+    <div className='items'>
+    <li>{text}</li>
+    <Tooltip title="Delete">
+      <Button className='dltbtn' onClick={()=>dltitems(id)}>
+         <DeleteIcon/>
+          </Button>
+        </Tooltip>
+    </div>
+    </>
   )
 }
 
